910f6c73bcdef0071aac985b4a0c2876f8fe8abf
Thant713 <59516322+Thant713@users.noreply.github.com>
2026-08-11 18:38:41 -0400

    Implement REPL with clean input command handling

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Fprintln(os.Stderr, "read error:", err)
+				os.Exit(1)
+			}
+			break
+		}
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
 }
 
 func cleanInput(text string) []string {
