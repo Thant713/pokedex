1b0fb6206826db0e0b937fa203caf4dfca005652
Thant713 <59516322+Thant713@users.noreply.github.com>
2026-08-13 21:52:04 -0400

    Add help command with dynamic usage listing

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ func main() {
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -31,8 +36,9 @@ func main() {
 		if len(words) == 0 {
 			continue
 		}
+
 		if cmd, ok := commands[words[0]]; ok {
-			if err := cmd.callback(); err != nil {
+			if err := cmd.callback(commands); err != nil {
 				fmt.Fprintln(os.Stderr, err)
 			}
 		} else {
@@ -44,15 +50,25 @@ func main() {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(commands map[string]cliCommand) error
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
-func commandExit() error {
+func commandExit(commands map[string]cliCommand) error {
 	fmt.Print("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
+	return nil
+}
+
+func commandHelp(commands map[string]cliCommand) error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for name, cmd := range commands {
+		fmt.Printf("%s: %s\n", name, cmd.description)
+	}
 	return nil
 }
